c8a177590e271e45f107d473575f6f2f99b3b065
hashfunc <root@hashfunc.io>
2021-10-04 19:30:15 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>hashfunc</title>
+      </Head>
+      <main>
+        <p>hashfunc</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
