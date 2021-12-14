0944748954012f93903b06c516c1c6b4796efb3c
hashfunc <root@hashfunc.io>
2021-12-14 21:18:24 +0900

    fix: next.config.js - delete useless config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const assetPrefix =
-  process.env.NODE_ENV === "production" ? "hashfunc.github.io" : "";
 module.exports = {
   reactStrictMode: true,
-  assetPrefix,
 };
