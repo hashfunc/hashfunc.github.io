c036daf07d49d5c184664ebc385f5c008db5ed94
hashfunc <hashfunc@users.noreply.github.com>
2023-12-30 13:03:59 +0000

    deploy: 1205295ed1d9698424967de9a5979340d8de47cb

---
+++ _next/static/udncCLuBhJcrq1LMTf_jL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-bbfc515a707920d9.js"],"/_error":["static/chunks/pages/_error-43be5540a4413675.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
